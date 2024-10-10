0a4db54d106360ea024a682c43ff5f57608e7a96
Guilherme Silva <silva.caldeira.guilherme@gmail.com>
2024-10-10 19:30:06 +0100

    ex02 done!

---
+++ ex02/Base.cpp
@@ -0,0 +1,60 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base::~Base( void ){
+	std::cout << "Base destroyed!\n\n";
+}
+
+Base * generate(void){
+	int	randomClass = rand() % 3 + 1;
+	if (randomClass == 1)
+		return (new A());
+	else if (randomClass == 2)
+		return (new B());
+	else if (randomClass == 3)
+		return (new C());
+	else
+		std::cout << "std::rand() Bad Usage!\n";
+	return (NULL);
+}
+
+void	identify(Base *p){
+	if (dynamic_cast<A *>(p) != NULL)
+		std::cout << "Base *p points to\tA" << std::endl;
+	else if (dynamic_cast<B *>(p) != NULL)
+		std::cout << "Base *p points to\tB" << std::endl;
+	else if (dynamic_cast<C *>(p) != NULL)
+		std::cout << "Base *p points to\tC" << std::endl;
+	else
+		std::cerr << "Something went wrong!\n";
+
+}
+
+void	identify(Base& p){
+	try
+	{
+		dynamic_cast<A &>(p);
+		std::cout << "Base &p points to\tA" << std::endl;
+		return ;
+	}
+	catch (const std::exception& e){(void) e;}
+	
+	try
+	{
+		dynamic_cast<B &>(p);
+		std::cout << "Base &p points to\tB" << std::endl;
+		return ;
+	}
+	catch (const std::exception& e){(void) e;}
+
+	try
+	{
+		dynamic_cast<C &>(p);
+		std::cout << "Base &p points to\tC" << std::endl;
+		return ;
+	}
+	catch (const std::exception& e){(void) e;}
+	std::cerr << "No Type identified!\n";
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,35 @@
+#include "Base.hpp"
+
+int main( int ac, char **av ){
+	(void)av;
+	int repeat = 1;
+
+	std::srand(std::time(0));
+	if (ac > 2)
+		std::cerr << "Wrong input!\n";
+	else
+	{
+		if (ac == 2)
+		{
+			if (atoi(av[1]) <= 0)
+			{
+				std::cerr << "Please input a number bigger than 0!\n";
+				exit(1);
+			}
+			repeat = atoi(av[1]);
+		}
+		for (;repeat > 0; repeat--)
+		{
+			Base *BasePointer = generate();
+			std::cout <<"Base Value:\t\t" << &BasePointer << "\n\n";
+
+			std::cout <<"Identifying...\nPointer:\t";
+			identify(BasePointer);
+
+			std::cout <<"Reference:\t";
+			identify(*BasePointer);
+
+			delete (BasePointer);
+		}
+	}
+}
